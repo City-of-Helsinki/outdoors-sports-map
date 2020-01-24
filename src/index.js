a4d51b6adb46f1a4b70f2202c56702f6b00c5715
jps <jpsalo@zoho.com>
2020-01-24 18:25:29 +0200

    Updated Babel presets. Upgraded deprecated polyfills to core-js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import {createElement} from 'react';
 import {render} from 'react-dom';
 import {browserHistory} from 'react-router';
