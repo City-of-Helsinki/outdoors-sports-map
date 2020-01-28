af2bec21a0b4a50a480cf0aac343e96dc2e968b6
jps <jpsalo@zoho.com>
2020-01-28 17:47:12 +0200

    Manually fixed or locally disabled ESLint errors (run from command line)

--- src/jest.setup.js
+++ src/jest.setup.js
@@ -4,6 +4,7 @@ import Adapter from 'enzyme-adapter-react-15';
 jest.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate HoC receive the t function as a prop
   translate: () => (Component) => {
+    // eslint-disable-next-line no-param-reassign
     Component.defaultProps = { ...Component.defaultProps, t: (k) => k };
     return Component;
   },
--- src/modules/home/components/Disclaimer.js
+++ src/modules/home/components/Disclaimer.js
@@ -5,8 +5,9 @@ import { translate } from 'react-i18next';
 export default translate()(({ attributionLink, t }) => (
   <div className="disclaimer">
     <div className="disclaimer__content">
+      {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
       <Link to="#">{t('APP.ABOUT')}</Link>
-      <a target="_blank" href={attributionLink}>
+      <a target="_blank" href={attributionLink} rel="noopener noreferrer">
         {t('MAP.ATTRIBUTION')}
         {' '}
       </a>
