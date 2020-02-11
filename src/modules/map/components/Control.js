City-of-Helsinki/outdoors-sports-map

commit 57ecbc8d604b9f461a58b771dc1087bdbd4566bc

Add UNSAFE_ prefix for deprecated lifecycle hooks (react-codemod). NOTE: ESLint camelcase rule accepts list of allowed strings but it would override Airbnb's rules so UNSAFE_ is allowed in files. See https://eslint.org/docs/rules/camelcase#allow

src/modules/map/components/Control.js
@@ -10,7 +10,8 @@ export default class Control extends MapControl { // note we're extending MapCon
     this.handleClick = this.handleClick.bind(this);
   }
 
-  componentWillMount() {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
     const { className, children } = this.props;
 
     const control = L.control({

src/modules/common/components/Root.js
@@ -9,7 +9,8 @@ class Root extends Component {
     this.state = { store: null };
   }
 
-  componentWillMount() {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
     createStore().then((store) => this.setState({ store }));
   }
 

src/modules/common/components/translation/TranslationProvider.js
@@ -37,14 +37,16 @@ const i18n = i18next
   });
 
 class TranslationProvider extends React.Component {
-  componentWillMount() {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
     if (this.props.language !== DEFAULT_LANG) {
       i18n.changeLanguage(this.props.language);
       this.forceUpdate();
     }
   }
 
-  componentWillReceiveProps(nextProps) {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(nextProps) {
     i18n.changeLanguage(nextProps.language);
   }
 

src/modules/home/components/HomeContainer.js
@@ -76,7 +76,8 @@ class HomeContainer extends Component<DefaultProps, Props, void> {
     };
   }
 
-  componentWillMount() {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillMount() {
     this.props.fetchUnits({});
     this.props.fetchServices();
 
@@ -98,7 +99,8 @@ class HomeContainer extends Component<DefaultProps, Props, void> {
     }
   }
 
-  componentWillReceiveProps(nextProps: Props) {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(nextProps: Props) {
     if (nextProps.activeLanguage !== this.props.activeLanguage) {
       this.forceUpdate();
     }

src/modules/unit/components/ListView.js
@@ -65,7 +65,8 @@ class ListView extends Component {
     this.loadMoreUnits = this.loadMoreUnits.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(nextProps) {
     if (!isEqual(nextProps.units, this.props.units) || !isEqual(nextProps.activeFilter, this.props.activeFilter)) {
       this.resetUnitCount();
     }

src/modules/unit/components/MapView.js
@@ -48,7 +48,8 @@ class MapView extends Component {
     window.addEventListener('resize', this.updateIsMobile);
   }
 
-  componentWillReceiveProps(nextProps) {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(nextProps) {
     const { selectedUnit } = this.props;
     if (nextProps.selectedUnit
       && (!selectedUnit || selectedUnit.id !== nextProps.selectedUnit.id)) {

src/modules/unit/components/UnitMarker.js
@@ -28,7 +28,8 @@ class UnitMarker extends Component {
     this.getIconHeight = this.getIconHeight.bind(this);
   }
 
-  componentWillReceiveProps(nextProps) {
+  // eslint-disable-next-line camelcase
+  UNSAFE_componentWillReceiveProps(nextProps) {
     const { isSelected } = this.props;
     if (!isSelected && nextProps.isSelected) {
       this.closePopup();

commit 814f313ba0911a59f83f3f9529e595e05081d6af

Fixed creating custom `react-leaflet` component broken in 57ecbc8d604b9f461a58b771dc1087bdbd4566bc.
This was working in `react-leaflet` v1.0.0 prior commit 57ecbc8d604b9f461a58b771dc1087bdbd4566bc where deprecated lifecycle hooks were prefixed with UNSAFE_

src/modules/map/components/Control.js
@@ -38,6 +38,14 @@ export default class Control extends MapControl { // note we're extending MapCon
     this.leafletElement = control;
   }
 
+  // `createLeafletElement` is mandatory for custom components
+  // TODO: Evaluate with `react-leaflet` v2 or when the logic for this component will change
+  //
+  // https://github.com/PaulLeCam/react-leaflet/blob/master/UPGRADING.md#v11
+  // https://react-leaflet.js.org/docs/en/v1/custom-components
+  // eslint-disable-next-line class-methods-use-this
+  createLeafletElement() {}
+
   handleClick(event) {
     L.DomEvent.stopPropagation(event);
     event.stopPropagation();