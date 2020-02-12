de7aeb83c6e97017ffdc86493b4940d78bb2083e
jps <jpsalo@zoho.com>
2020-02-12 15:24:23 +0200

    Bump test configuration to react v16

--- src/jest.setup.js
+++ src/jest.setup.js
@@ -1,5 +1,5 @@
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
+import Adapter from 'enzyme-adapter-react-16';
 
 jest.mock('react-i18next', () => ({
   // this mock makes sure any components using the translate HoC receive the t function as a prop
