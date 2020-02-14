837db4fac5e7064d7fff9aec2886c1ef75088321
jps <jpsalo@zoho.com>
2020-02-14 16:49:51 +0200

    Bump redux-persist from 4.0.0-beta1 to 5.10.0 and migrate to v5

--- src/bootstrap/createStore.js
+++ src/bootstrap/createStore.js
@@ -1,6 +1,7 @@
 import { applyMiddleware, compose, createStore as rawCreateStore } from 'redux';
-import { persistStore, autoRehydrate } from 'redux-persist';
+import { persistReducer, persistStore } from 'redux-persist';
 import createSagaMiddleware from 'redux-saga';
+import storage from 'redux-persist/lib/storage';
 import createRootReducer from './createRootReducer';
 import rootSaga from './rootSaga';
 import { APP_NAME } from '../modules/common/constants';
@@ -9,26 +10,34 @@ import { APP_NAME } from '../modules/common/constants';
  * @returns {function}
  */
 const createStore = () => new Promise((resolve) => {
-  const rootReducer = createRootReducer();
-  const sagaMiddleware = createSagaMiddleware();
-  const middlewares = [];
+  const persistConfig = {
+    key: 'primary',
+    storage,
+    whitelist: ['language', 'map'],
+    blacklist: [],
+    keyPrefix: `${APP_NAME}:`,
+  };
 
+  const rootReducer = createRootReducer();
+  const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+  const middlewares = [];
+  const sagaMiddleware = createSagaMiddleware();
   middlewares.push(sagaMiddleware);
 
   const enhancer = compose(
     applyMiddleware(...middlewares),
-    autoRehydrate(),
     window.devToolsExtension ? window.devToolsExtension() : (f) => f,
   );
 
-  const store = rawCreateStore(rootReducer, enhancer);
+  const store = rawCreateStore(persistedReducer, enhancer);
 
   // The promise returned by "createStore" will be resolved once we have re-hydrated the state.
-  persistStore(store, {
-    whitelist: ['language', 'map'],
-    blacklist: [],
-    keyPrefix: `${APP_NAME}:`,
-  }, () => resolve(store));
+  persistStore(
+    store,
+    null,
+    () => resolve(store),
+  );
 
   sagaMiddleware.run(rootSaga);
 });
