City-of-Helsinki/outdoors-sports-map

commit d1cdab2c779bc0da2db9b8dfbe28711f609c5424

Added hooks support to `react-hot-loader`

config/webpack/common.js
@@ -20,6 +20,7 @@ module.exports = {
     ],
     extensions: ['.js'],
     alias: {
+      'react-dom': '@hot-loader/react-dom', // hooks support for `react-hot-loader`
       '@assets': path.join(context, 'assets'),
     },
   },

commit 5e06cab11594a1e7bf9fa1240cbc9ca1c10d9d4b

Fixed tests setup broken in d1cdab2c779bc0da2db9b8dfbe28711f609c5424

config/webpack/common.js
@@ -20,7 +20,6 @@ module.exports = {
     ],
     extensions: ['.js'],
     alias: {
-      'react-dom': '@hot-loader/react-dom', // hooks support for `react-hot-loader`
       '@assets': path.join(context, 'assets'),
     },
   },