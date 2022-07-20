cf0a19d0f7b2ad2f938db1086ce87b4a5f7bfe02
Mika Hiltunen <github@haaska.net>
2022-07-20 07:10:30 +0300

    Update SVG and PNG icons
    SVG files were converted to PNG
    using rsvg-convert

---
+++ src/domain/assets/markers/create_pngs.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+rsvg-convert -h 32 iceskate-unusable-off.svg > iceskate-unusable-off.png
+rsvg-convert -h 64 iceskate-unusable-off.svg > iceskate-unusable-off@2x.png
+rsvg-convert -h 32 iceskate-unusable-on.svg > iceskate-unusable-on.png
+rsvg-convert -h 64 iceskate-unusable-on.svg > iceskate-unusable-on@2x.png
+
+rsvg-convert -h 32 ski-unusable-off.svg > ski-unusable-off.png
+rsvg-convert -h 64 ski-unusable-off.svg > ski-unusable-off@2x.png
+rsvg-convert -h 32 ski-unusable-on.svg > ski-unusable-on.png
+rsvg-convert -h 64 ski-unusable-on.svg > ski-unusable-on@2x.png
+
+rsvg-convert -h 32 swim-unusable-off.svg > swim-unusable-off.png
+rsvg-convert -h 64 swim-unusable-off.svg > swim-unusable-off@2x.png
+rsvg-convert -h 32 swim-unusable-on.svg > swim-unusable-on.png
+rsvg-convert -h 64 swim-unusable-on.svg > swim-unusable-on@2x.png
