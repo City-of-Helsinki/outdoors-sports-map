f69c25df31b04312f8dcfa7b9e3f83a32128e6ec
Awel Eshetu <awel.eshetu@anders.com>
2023-03-31 14:19:28 +0300

    Zoom to unit when it is selected
     - zooms in to selected unit when clicked from search suggestions

--- src/domain/map/mapConstants.ts
+++ src/domain/map/mapConstants.ts
@@ -22,6 +22,8 @@ export const MIN_ZOOM = 11;
 
 export const MAX_ZOOM = 18;
 
+export const DETAIL_ZOOM_IN = 14;
+
 export const BOUNDARIES: LatLngBoundsLiteral = [
   [59.4, 23.8],
   [61.5, 25.8],
