bb3cac1fd42468bf574971c35ab5d35fa8e80e8f
awel1122 <117433931+awel1122@users.noreply.github.com>
2023-04-04 16:47:13 +0300

    Zoom to unit when it is selected (#207)
    - zooms in to selected unit when clicked from search suggestions

--- src/domain/home/HomeContainer.tsx
+++ src/domain/home/HomeContainer.tsx
@@ -9,6 +9,7 @@ import AppInfoDropdown from "../app/AppInfoDropdown";
 import routerPaths from "../app/appRoutes";
 import useIsUnitBrowserSearchView from "../app/useIsUnitBrowserSearchView";
 import Map from "../map/Map";
+import { DETAIL_ZOOM_IN } from "../map/mapConstants";
 import UnitBrowser from "../unit/browser/UnitBrowser";
 import UnitDetails from "../unit/details/UnitDetails";
 import { getUnitPosition } from "../unit/unitHelpers";
@@ -92,6 +93,23 @@ function HomeContainer() {
 
         if (adjustedCenter) {
           leafletElement?.setView(adjustedCenter);
+          //if the geometry is a MultiLineString, then zoom in to the bounds of the geometry
+          if(unit.geometry.type === 'MultiLineString') {
+            //get a flat array of coordinates
+            const coordinates = unit.geometry.coordinates.flat();
+            //get the maximum latitude and longitude
+            const maxLat = Math.max(...coordinates.map((coord:number[]) => coord[0])),
+            minLat = Math.min(...coordinates.map((coord:number[]) => coord[0])),
+            maxLng = Math.max(...coordinates.map((coord:number[]) => coord[1])),
+            minLng = Math.min(...coordinates.map((coord:number[]) => coord[1]));
+            //set the zoom and fit the bounds of the coordinates
+            leafletElement?.setZoom(DETAIL_ZOOM_IN);
+            leafletElement?.fitBounds([[minLat, minLng], [maxLat, maxLng]]);
+            //if the geometry is a point, then zoom in to that point
+          }else{
+            const coordinates = unit.location.coordinates;
+            leafletElement?.flyTo([coordinates[1], coordinates[0]], DETAIL_ZOOM_IN);
+          }
         }
       } else {
         // On mobile we want to move the map 250px down from the center, so the
