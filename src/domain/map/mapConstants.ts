0a2627f64b42ba1fc9545c822aab6a8a27a92e2b
Mika Hietanen <m.hietanen84@gmail.com>
2024-06-10 12:15:03 +0300

    Adjust map zoom constants
    Updates:
    - Increase detail zoom in level to 15. This will remove previous map jumps different place.
    - Decrease min zoom to level 10. This will allow user to zoom farther away
    to see more at once with smaller screens.
    
    Refs HUL-16

--- src/domain/map/mapConstants.ts
+++ src/domain/map/mapConstants.ts
@@ -18,11 +18,11 @@ export const MAP_RETINA_URL = {
 
 export const DEFAULT_ZOOM = 12;
 
-export const MIN_ZOOM = 11;
+export const MIN_ZOOM = 10;
 
 export const MAX_ZOOM = 18;
 
-export const DETAIL_ZOOM_IN = 14;
+export const DETAIL_ZOOM_IN = 15;
 
 export const BOUNDARIES: LatLngBoundsLiteral = [
   [59.4, 23.8],
