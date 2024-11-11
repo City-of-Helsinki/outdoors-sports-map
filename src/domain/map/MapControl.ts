7da0fde6f7a61b9527036f57904251f57aa710ce
Mika Hietanen <m.hietanen84@gmail.com>
2024-11-11 13:58:17 +0200

    Update code formatting

--- src/domain/map/MapControl.ts
+++ src/domain/map/MapControl.ts
@@ -11,29 +11,29 @@ type Props = {
 };
 
 const createMapControl = (props: Props) => {
-    const { className, children, handleClick, position } = props;
+  const { className, children, handleClick, position } = props;
 
-    const control = new L.Control({
-      position: position || "bottomright",
-    });
-  
-    control.onAdd = () => {
-      const div = L.DomUtil.create("div", `custom-control ${className}`);
-      const link = L.DomUtil.create("button", "custom-control-button", div);
-  
-      L.DomEvent.on(link, "mousedown dblclick", L.DomEvent.stopPropagation)
-        .on(link, "click", L.DomEvent.stop)
-        .on(link, "click", (event: Event) => {
-          L.DomEvent.stopPropagation(event);
-          handleClick && handleClick(event);
-        });
-  
-      ReactDOM.render(children, link);
-  
-      return div;
-    };
-  
-    return control;
+  const control = new L.Control({
+    position: position || "bottomright",
+  });
+
+  control.onAdd = () => {
+    const div = L.DomUtil.create("div", `custom-control ${className}`);
+    const link = L.DomUtil.create("button", "custom-control-button", div);
+
+    L.DomEvent.on(link, "mousedown dblclick", L.DomEvent.stopPropagation)
+      .on(link, "click", L.DomEvent.stop)
+      .on(link, "click", (event: Event) => {
+        L.DomEvent.stopPropagation(event);
+        handleClick && handleClick(event);
+      });
+
+    ReactDOM.render(children, link);
+
+    return div;
+  };
+
+  return control;
 };
 
 const MapControl = createControlComponent(createMapControl);
