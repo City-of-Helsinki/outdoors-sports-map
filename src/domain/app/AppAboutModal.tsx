fc8f20e9c351dd3bcaa8531ce16b49d725368424
Jori Lindell <jori.lindell@gmail.com>
2025-11-26 16:05:35 +0200

    chore: replace Bootstrap Modal with HDS Dialog in AppAboutModal
    - Convert AppAboutModal from react-bootstrap Modal to HDS Dialog component
    - Update AppInfoDropdown to pass focusAfterCloseRef to AppAboutModal
    - Add timeout cleanup logic to prevent state updates on unmounted component
    - Adjust z-index values for proper layering (map-foreground: 798, info dropdown: 792)

--- src/domain/app/AppAboutModal.tsx
+++ src/domain/app/AppAboutModal.tsx
@@ -1,31 +1,48 @@
-import Modal from "react-bootstrap/Modal";
+import {Button, Dialog} from "hds-react";
+import { RefObject } from "react";
 import { useTranslation } from "react-i18next";
 
+import { MODAL_WIDTH } from "../constants";
+
 type Props = {
+  focusAfterCloseRef?: RefObject<HTMLElement>;
   show: boolean;
   onClose: () => void;
 };
 
-function AppAboutModal({ show = false, onClose }: Props) {
+function AppAboutModal({ focusAfterCloseRef, show = false, onClose }: Props) {
   const { t } = useTranslation();
+  const titleId = "about-dialog-title";
 
   return (
-    <Modal show={show} onHide={onClose} size="lg" animation={false}>
-      <Modal.Header closeButton closeLabel={t("APP.MODAL.CLOSE")}>
-        <Modal.Title>
-          <h2>{t("APP.NAME")}</h2>
-        </Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <div
-          // eslint-disable-next-line react/no-danger
-          dangerouslySetInnerHTML={{
-            __html: t("APP.ABOUT_MODAL"),
-          }}
+    <Dialog
+        id="about-dialog"
+        aria-labelledby={titleId}
+        isOpen={show}
+        close={onClose}
+        closeButtonLabelText={t("APP.MODAL.CLOSE")}
+        focusAfterCloseRef={focusAfterCloseRef}
+        style={{width: MODAL_WIDTH}}
+      >
+        <Dialog.Header
+          id={titleId}  
+          title={t("APP.NAME")}
         />
-      </Modal.Body>
-    </Modal>
-  );
+        <Dialog.Content>
+          <div
+            // eslint-disable-next-line react/no-danger
+            dangerouslySetInnerHTML={{
+              __html: t("APP.ABOUT_MODAL"),
+            }}
+          />
+        </Dialog.Content>
+        <Dialog.ActionButtons>
+          <Button onClick={onClose}>
+            {t("APP.MODAL.CLOSE")}
+          </Button>
+        </Dialog.ActionButtons>
+      </Dialog>
+  )
 }
 
 export default AppAboutModal;
